f1833a9744f7bf13a80da9e05a4042f87109db63
skd1993 <shobhit.kr@outlook.com>
2021-12-25 19:59:36 +0530

    bump versionCode, targetSDK 30, hide miliseconds, hex converter, copy hex and epoch, share all. decouple date and time'

--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,5 +1,5 @@
 import React, { Fragment, useEffect, useState } from 'react';
-import { StyleSheet, ScrollView, View, Text, ViewStyle, TextInput, TextStyle, ToastAndroid, Linking, TouchableOpacity, Platform, Share } from 'react-native';
+import { StyleSheet, ScrollView, View, Text, ViewStyle, TextInput, TextStyle, ToastAndroid, Linking, TouchableOpacity, Platform, Share, Switch } from 'react-native';
 import Clipboard from 'expo-clipboard';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { MaterialIcons } from '@expo/vector-icons';
@@ -26,23 +26,40 @@ const Main = (): JSX.Element => {
   const [mode, setMode] = useState<'date' | 'time'>('date');
   const [show, setShow] = useState<boolean>(false);
   const [err, setErr] = useState<boolean>(false);
+  const [showms, setShowms] = useState<boolean>(true);
 
   useEffect(() => {
     let epochTimer = setInterval(() => {
       setDt(new Date())
     }, 1)
     return () => clearInterval(epochTimer);
-  }, []);
+  }, [showms]);
 
   function generateRandomTimeHandler(start?: Date, end?: Date): void {
     const randTime = randDateEpoch(start, end);
     setDatePickerVal(randTime.randDate);
   }
 
+  const toggleMsView = (): void => {
+    setShowms(!showms);
+  }
+
   const onChange = (event: Event, selectedDate: Date): void => {
     const currentDate: Date = selectedDate || new Date();
-    setShow(Platform.OS === 'ios');
-    setDatePickerVal(currentDate);
+    if (!datePickerVal) setDatePickerVal(currentDate);
+    else {
+      if (mode === 'time') {
+        const toSet = new Date(datePickerVal.getFullYear(), datePickerVal.getMonth(), datePickerVal.getDate(),
+          currentDate.getHours(), currentDate.getMinutes(), currentDate.getSeconds());
+        setDatePickerVal(toSet);
+      }
+      else if (mode === 'date') {
+        const toSet = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate(),
+          datePickerVal.getHours(), datePickerVal.getMinutes(), datePickerVal.getSeconds());
+        setDatePickerVal(toSet);
+      }
+    }
+    setShow(false);
   };
 
   const showMode = (currentMode: 'time' | 'date'): void => {
@@ -87,6 +104,11 @@ const Main = (): JSX.Element => {
     }
   }
 
+  const hexInputChangeHandler = (text: string): void => {
+    const toPass = parseInt(text, 16).toString();
+    epochInputChangeHandler(toPass);
+  }
+
   return (
     <ScrollView contentContainerStyle={styles.mainContainer} endFillColor="#fff" showsVerticalScrollIndicator={false}>
       {React.useMemo(() => {
@@ -94,18 +116,31 @@ const Main = (): JSX.Element => {
       }, [show])}
       <Card title="Current Epoch">
         <View style={styles.cardContent}>
-          <Text style={styles.mainTextStyle}>{dt.getTime()}</Text>
+          <Text style={styles.mainTextStyle}>{showms ? dt.getTime() : (dt.getTime().toString().slice(0, -3))}</Text>
           <Text style={styles.subTextStyle}>{`(${dt.toString()})`}</Text>
+          <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 15 }}><Text style={styles.subTextStyle}>Show milliseconds</Text><Switch value={showms} onValueChange={toggleMsView} /></View>
         </View>
       </Card>
       <Card title="Epoch Converter">
         <Fragment>
           <View style={styles.cardContent}>
+
             <View style={[styles.inputContainer, { flexDirection: 'row', alignItems: 'center' }]}>
               <TextInput style={styles.inputContent} placeholder="Enter Epoch Value" keyboardType="numeric" value={datePickerVal?.getTime().toString()} maxLength={15} onChangeText={epochInputChangeHandler}></TextInput>
-              {datePickerVal && <TouchableOpacity onPress={() => setDatePickerVal(undefined)}><View><MaterialIcons name='clear' size={25} color='#7f8c8d' /></View></TouchableOpacity>}
+              {datePickerVal && <Fragment><TouchableOpacity onPress={() => {
+                Clipboard.setString(datePickerVal.getTime().toString());
+                ToastAndroid.showWithGravity(
+                  'Copied epoch to clipboard!',
+                  ToastAndroid.SHORT,
+                  ToastAndroid.BOTTOM
+                );
+              }}><View><MaterialIcons name='content-copy' size={20} color='#7f8c8d' /></View></TouchableOpacity><TouchableOpacity onPress={() => setDatePickerVal(undefined)}><View><MaterialIcons name='clear' size={20} color='#7f8c8d' /></View></TouchableOpacity></Fragment>}
             </View>
+
+            {err && <Text>Please enter a valid number</Text>}
+
             <View style={{ height: 10 }}></View>
+
             <View style={{ flexDirection: 'row' }}>
               <View style={[styles.inputContainer, { flex: 1 }]}>
                 <TouchableOpacity onPress={showDatepicker}>
@@ -119,17 +154,30 @@ const Main = (): JSX.Element => {
                 </TouchableOpacity>
               </View>
             </View>
-            {err && <Text>Please enter a valid number</Text>}
-            <View style={{ flexDirection: 'row' }}>
-              <CustomButton onPress={() => generateRandomTimeHandler()} icon='autorenew' iconStyle={{}} title="RANDOM" />
-              {datePickerVal && <Fragment><View style={{ width: 5 }}></View><CustomButton buttonContainerStyle={{ flex: 0.20 }} buttonStyle={{ backgroundColor: '#ecf0f1' }} iconStyle={{ color: '#000' }} onPress={() => {
-                Clipboard.setString(datePickerVal.getTime().toString());
+
+            <View style={{ height: 10 }}></View>
+
+            <View style={[styles.inputContainer, { flexDirection: 'row', alignItems: 'center' }]}>
+              <TextInput style={styles.inputContent} placeholder="Enter Hex Value" value={datePickerVal?.getTime().toString(16).toUpperCase()} onChangeText={hexInputChangeHandler}></TextInput>
+              {datePickerVal && <Fragment><TouchableOpacity onPress={() => {
+                Clipboard.setString(datePickerVal.getTime().toString(16).toUpperCase());
                 ToastAndroid.showWithGravity(
-                  'Copied epoch to clipboard!',
+                  'Copied hex to clipboard!',
                   ToastAndroid.SHORT,
                   ToastAndroid.BOTTOM
                 );
-              }} icon="content-copy" /></Fragment>}
+              }}><View><MaterialIcons name='content-copy' size={20} color='#7f8c8d' /></View></TouchableOpacity><TouchableOpacity onPress={() => setDatePickerVal(undefined)}><View><MaterialIcons name='clear' size={20} color='#7f8c8d' /></View></TouchableOpacity></Fragment>}
+            </View>
+
+            <View style={{ flexDirection: 'row' }}>
+              <CustomButton onPress={() => generateRandomTimeHandler()} icon='autorenew' iconStyle={{}} title="RANDOM" />
+              {datePickerVal && <Fragment><View style={{ width: 5 }}></View><CustomButton buttonStyle={{ backgroundColor: '#ecf0f1' }} iconStyle={{ color: '#000' }} textStyle={{ color: '#000' }} onPress={() => {
+                const toCopy = `Epoch:\n${datePickerVal.getTime().toString()}\nDate time:\n${datePickerVal.toString()}\nHex:\n${datePickerVal.getTime().toString(16).toUpperCase()}`;
+                Share.share({
+                  title: 'Epoch',
+                  message: toCopy,
+                })
+              }} icon="share" title="Share All " /></Fragment>}
             </View>
             {/* <View style={{ flexDirection: 'row' }}>
               <CustomButton onPress={() => {}} icon='share' buttonContainerStyle={{flex: 1}} buttonStyle={{backgroundColor: '#651FFF'}} title="Share" />
--- src/utils/randDateEpoch.ts
+++ src/utils/randDateEpoch.ts
@@ -1,6 +1,6 @@
 export function randDateEpoch(
   start = new Date(1970, 0, 1),
-  end = new Date()
+  end = new Date((new Date()).getFullYear()+100 , 0, 1)
 ): { randEpoch: Number; randDate: Date } {
   const randEpoch = Math.floor(
     start.getTime() + Math.random() * (end.getTime() - start.getTime())
